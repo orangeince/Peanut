3653714cc9cabf4d1f7b9003c19317e0ed376fff
Shao <orangeince@outlook.com>
2018-12-16 23:43:24 +0800

    chg: 优化点击更改主题后cell的checkmark状态

--- Peanut/Settings/ThemeSettingViewController.swift
+++ Peanut/Settings/ThemeSettingViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import Gestalt
 
-class ThemeSettingViewController: ItemsViewController<UITableViewCell, AppTheme> {
+class ThemeSettingViewController: ItemsViewController<ThemeItemCell, AppTheme> {
     let current = ThemeManager.default.theme
+    
     init() {
         super.init(items: [.light, .dark]) { (cell, theme) in
             if theme == AppTheme.light {
@@ -20,15 +21,38 @@ class ThemeSettingViewController: ItemsViewController<UITableViewCell, AppTheme>
             }
             if let current = ThemeManager.default.theme as? AppTheme,
                 current == theme {
-                cell.accessoryType = .checkmark
+                cell.isSelected = true
             } else {
-                cell.accessoryType = .none
+                cell.isSelected = false
             }
         }
         didSelectedItem = { theme in
             guard let current = ThemeManager.default.theme as? AppTheme,
                 current != theme else { return }
             ThemeManager.default.theme = theme
+        }
+        tableView.rowHeight = 56
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        didSelectedItem?(items[indexPath.row])
+        tableView.reloadData()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class ThemeItemCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.selectionStyle = .none
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            accessoryType = isSelected ? .checkmark : .none
         }
     }
     
